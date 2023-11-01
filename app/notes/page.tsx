b22bb63889727a7e320598be7fffc65a2c27951d
You-Keitou <youkeitou327@gmail.com>
2023-11-02 08:40:00 +0900

    supabase cli npmで導入

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,37 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import { Suspense } from "react";
 
-export default async function Page() {
+type Note = {
+  id: string;
+  title: string;
+};
+
+async function NoteLists() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data: notes } = await supabase.from("notes").select();
+  const { data: notes } = (await supabase.from("notes").select()) as {
+    data: Note[];
+  };
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      <h1 className="text-4xl font-bold">Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <h2 className="text-2xl font-bold">{note.title}</h2>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function Notes() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <NoteLists />
+    </Suspense>
+  );
 }
