5a0dde6d01f6fb6c27c3692263e774e451ef96dc
utaash88 <hanamoriguchi@gmail.com>
2023-11-04 09:56:37 +0900

    日記作成雛形

---
+++ app/making-diaries/page.tsx
@@ -0,0 +1,13 @@
+import "./making-diaries.css"
+export default function makeDiaries(){
+    return(
+        <div className = "body">
+            <div class
+            <div className = "diaries">
+                <input type="date" name="sample"></input>
+                <textarea placeholder="今日はどんな1日でしたか？"></textarea>
+                <input type="submit" value="送信"></input>
+            </div>
+        </div>
+    )
+}
