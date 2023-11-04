4b6afdab63fb1a68f0ffceb833a54cd54ea3c69b
You-Keitou <youkeitou327@gmail.com>
2023-11-04 11:39:35 +0900

    タイポ修正

--- app/making-diaries/page.tsx
+++ app/making-diaries/page.tsx
@@ -1,13 +1,12 @@
 import "./making-diaries.css"
-export default function makeDiaries(){
-    return(
-        <div className = "body">
-            <div class
-            <div className = "diaries">
-                <input type="date" name="sample"></input>
-                <textarea placeholder="今日はどんな1日でしたか？"></textarea>
-                <input type="submit" value="送信"></input>
-            </div>
-        </div>
-    )
+export default function makeDiaries() {
+  return (
+    <div className="body">
+      <div className="diaries">
+        <input type="date" name="sample"></input>
+        <textarea placeholder="今日はどんな1日でしたか？"></textarea>
+        <input type="submit" value="送信"></input>
+      </div>
+    </div>
+  )
 }
