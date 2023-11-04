593d22b19b3c4dde74557864c01bfdea7a5212ba
You-Keitou <youkeitou327@gmail.com>
2023-11-04 14:25:31 +0900

    hotfix: headerのバグ

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,3 @@
-import NextLogo from './NextLogo'
-import SupabaseLogo from './SupabaseLogo'
-
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
@@ -10,16 +7,16 @@ export default function Header() {
           target="_blank"
           rel="noreferrer"
         >
-          <SupabaseLogo />
+          {/* <SupabaseLogo /> */}
         </a>
         <span className="border-l rotate-45 h-6" />
         <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
+          {/* <NextLogo /> */}
         </a>
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{' '}
+        The fastest way to build apps with{" "}
         <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
@@ -27,8 +24,8 @@ export default function Header() {
           rel="noreferrer"
         >
           Supabase
-        </a>{' '}
-        and{' '}
+        </a>{" "}
+        and{" "}
         <a
           href="https://nextjs.org/"
           target="_blank"
