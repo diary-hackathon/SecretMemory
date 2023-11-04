771b5fe7e0437094ee879754f092124da518127a
koki-HT <kishi.koki6@gmail.com>
2023-11-04 16:24:42 +0900

    質問をランダムに5件出す

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react';
+import seedrandom from 'seedrandom';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -8,40 +9,60 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const TABLE_NAME = 'questions'
 
-export const getList = async () => {
+// 今日の日付をシード値として使用
+const seed = new Date().toISOString().slice(0,10); // YYYY-MM-DD format
+const rng = seedrandom(seed);
+
+// 全てのIDを取得する関数
+const getAllQuestionIds = async () => {
+    const { data, error } = await supabase.from(TABLE_NAME).select('id');
+    if (error) throw new Error(error);
+    return data.map(item => item.id);
+};
+
+// シード値を基にランダムなIDを5つ選択する関数
+const selectRandomIds = (allIds) => {
+    const shuffled = allIds.sort(() => 0.5 - rng());
+    return shuffled.slice(0, 5);
+};
+
+export const getRandomQuestions = async () => {
     try {
+        const allIds = await getAllQuestionIds();
+        const randomSelectedIds = selectRandomIds(allIds);
+        console.log(randomSelectedIds);
         const { data, error } = await supabase
-          .from(TABLE_NAME)
-          .select('*')
-          .order('created_at')
-  
-        if (error) throw new Error(error)
-  
-        return data
-      } catch (e) {
-          console.error(e.message)
-      }
-  };
+            .from(TABLE_NAME)
+            .select('*')
+            .in('id', randomSelectedIds);
+
+        if (error) throw new Error(error);
+        
+        return data;
+    } catch (e) {
+        console.error(e.message);
+        return [];
+    }
+};
 
 export default function questionPage(){
     const [questions, setQuestions] = useState([]);
-    
+    console.log(`fetchData: ${JSON.stringify(questions, null, 2)}`);
     useEffect(() => {
         const fetchData = async () => {
-            const data = await getList();
+            const data = await getRandomQuestions();
             setQuestions(data);
-            console.log(data);
         };
-
         fetchData();
     }, []);
+    
 
-      const handleAnswerChange = (id, answer) => {
+    const handleAnswerChange = (id, answer) => {
         const updatedQuestions = questions.map(q => 
-          q.id === id ? { ...q, answer } : q
+            q.id === id ? { ...q, answer } : q
         );
         setQuestions(updatedQuestions);
-      };
+    };
     return(
         <div className = "body">
             <div className="py-12">
