8761d37c07310fba95a709395eb595059031f655
You-Keitou <youkeitou327@gmail.com>
2023-11-05 13:07:12 +0900

    型最終調整

--- app/(authticated)/questions/page.tsx
+++ app/(authticated)/questions/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 
+import { createClient } from "@supabase/supabase-js"
 import { useState, useEffect } from "react"
 import seedrandom from "seedrandom"
 
-import { createClient } from "@/utils/supabase/client"
+import type { Database } from "@/types/supabase"
 
-const saveAnswers = async (answers) => {
+const saveAnswers = async (
+  answers: Database["public"]["Tables"]["answers"]["Row"][]
+) => {
   try {
-    const supabase = createClient()
+    const supabase = createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+    )
     const user_id = (await supabase.auth.getUser()).data.user?.id // 現在のユーザーを取得
 
     if (!user_id) {
@@ -39,14 +45,17 @@ const rng = seedrandom(seed)
 
 // 全てのIDを取得する関数
 const getAllQuestionIds = async () => {
-  const supabase = createClient()
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  )
   const { data, error } = await supabase.from(TABLE_NAME).select("id")
   if (error) return []
   return data.map((item) => item.id)
 }
 
 // シード値を基にランダムなIDを5つ選択する関数
-const selectRandomIds = (allIds) => {
+const selectRandomIds = (allIds: string[]) => {
   const shuffled = allIds.sort(() => 0.5 - rng())
   return shuffled.slice(0, 5)
 }
@@ -55,8 +64,10 @@ const getRandomQuestions = async () => {
   try {
     const allIds = await getAllQuestionIds()
     const randomSelectedIds = selectRandomIds(allIds)
-    const supabase = createClient()
-    // console.log(randomSelectedIds);
+    const supabase = createClient<Database>(
+      process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+    )
     const { data, error } = await supabase
       .from(TABLE_NAME)
       .select("*")
@@ -72,7 +83,11 @@ const getRandomQuestions = async () => {
 }
 
 export default function questionPage() {
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState<
+    Database["public"]["Tables"]["questions"]["Row"][]
+  >([])
+  const [user_id, setUser_id] = useState<string>("")
+  const [user_answer, setUserAnswer] = useState<string>("")
   // console.log(`fetchData: ${JSON.stringify(questions, null, 2)}`);
   useEffect(() => {
     const fetchData = async () => {
@@ -82,14 +97,54 @@ export default function questionPage() {
     fetchData()
   }, [])
 
-  const handleAnswerChange = (id, answer) => {
+  const handleAnswerChange = (id: string, answer: string) => {
     const updatedQuestions = questions.map((q) =>
       q.id === id ? { ...q, answer } : q
     )
     setQuestions(updatedQuestions)
   }
+
+  const saveAnswers = async (
+    answers: {
+      id: string
+      answer: string
+      question_id: string
+      user_id: string
+      written_date: string
+    }[]
+  ) => {
+    try {
+      const supabase = createClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+      )
+      // answers配列の各項目に対してデータベースに保存する処理
+      for (const answer of answers) {
+        const { error } = await supabase.from("answers").insert({
+          user_id: answer.user_id,
+          question_id: answer.question_id,
+          written_date: answer.written_date,
+          answer: answer.answer
+        })
+        if (error) {
+          throw error
+        }
+      }
+    } catch (error) {
+      console.error("Error saving answers:", error)
+    }
+  }
+
   const handleSubmit = async () => {
-    await saveAnswers(questions)
+    await saveAnswers(
+      questions.map((q) => ({
+        id: q.id,
+        answer: user_answer,
+        question_id: q.id,
+        user_id: user_id,
+        written_date: seed
+      }))
+    )
     // 保存後の処理（例：ユーザーに成功メッセージを表示するなど）をここに追加できます。
   }
   return (
@@ -107,13 +162,11 @@ export default function questionPage() {
                     <div className="text-center">
                       <textarea
                         className="w-full"
-                        rows="3"
+                        rows={3}
                         onChange={(e) =>
                           handleAnswerChange(question.id, e.target.value)
                         }
-                      >
-                        {question.answer}
-                      </textarea>
+                      ></textarea>
                     </div>
                   </div>
                 ))}
